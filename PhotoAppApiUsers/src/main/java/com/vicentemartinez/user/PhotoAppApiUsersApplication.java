ab4625e36e263ebc98d473ea5c88066cb039d7cc
Eduardo Vicente <eduardo.vicente@outlook.es>
2019-10-31 16:56:48 -0600

    adding progress

--- PhotoAppApiUsers/src/main/java/com/vicentemartinez/user/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/vicentemartinez/user/PhotoAppApiUsersApplication.java
@@ -1,8 +1,10 @@
-package com.vicentemartinez.user.PhotoAppApiUsers;
+package com.vicentemartinez.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotoAppApiUsersApplication {
 
