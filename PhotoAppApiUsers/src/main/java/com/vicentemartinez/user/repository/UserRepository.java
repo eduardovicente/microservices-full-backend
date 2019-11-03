e8bb1e4eece9f35d4a50a03ea61b930dd69d231f
eduardovicente <eduardo.vicente@outlook.es>
2019-11-03 13:33:02 -0600

    User Repository method

--- PhotoAppApiUsers/src/main/java/com/vicentemartinez/user/repository/UserRepository.java
+++ PhotoAppApiUsers/src/main/java/com/vicentemartinez/user/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.vicentemartinez.user.model.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {}
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+	UserEntity findByEmail(String email);
+}
