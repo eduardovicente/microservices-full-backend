aafef765c755f8a0d27e012ca45fb270908ed605
eduardovicente <eduardo.vicente@outlook.es>
2019-11-20 23:36:59 -0600

    Add new query to user repository

--- PhotoAppApiUsers/src/main/java/com/vicentemartinez/user/repository/UserRepository.java
+++ PhotoAppApiUsers/src/main/java/com/vicentemartinez/user/repository/UserRepository.java
@@ -8,4 +8,5 @@ import com.vicentemartinez.user.model.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
+	UserEntity findByUserId(String userId);
 }
